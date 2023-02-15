143d1f5877fc183b4d85107f9627d8cbadd968d3
Mihail <dazzzz2007@gmail.com>
2023-02-15 17:26:48 +0200

    Add middleware to paths

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 const passport = require("passport");
 
-const router = Router();
+const userContext = require("$middleware/userContext");
 
-const userContext = require("../middleware/userContext");
+const router = Router();
 
 const secureRoutes = require("./secure");
 const publicRoutes = require("./public");
